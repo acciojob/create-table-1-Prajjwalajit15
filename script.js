27628601c6dea7cdf93a91172203e5671e55af96
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-09-21 21:28:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  const col1 = document.createElement("td");
+  const col2 = document.createElement("td");
+
+  col1.innerHTML = "New Cell1";
+  col2.innerHTML = "New Cell2";
+
+  const table = document.getElementById("sampleTable");
+
+  const newRow = document.createElement("tr");
+  newRow.appendChild(col1);
+  newRow.appendChild(col2);
+
+  // Insert the new row at the top of the table
+  if (table.firstChild) {
+    table.insertBefore(newRow, table.firstChild);
+  } else {
+    table.appendChild(newRow);
+  }
 }
+
